3db78ba1e137f9bd61eee89c9bfd7d99397faedf
Munis <xamrakulovmunis@gmail.com>
2022-11-24 10:16:59 +0500

    new commit

---
+++ script5.js
@@ -0,0 +1,48 @@
+// N.1
+
+// let number = prompt('Any number?')
+
+// if (number % 2 === 0) {
+//     alert('Четное')
+// } else {
+//     alert('Не четное')
+// }
+
+
+
+// N.2
+
+// let userName = prompt('What is your name?').trim().toUpperCase()
+// let userAge = prompt('How old are you?').trim()
+
+// if (userName.length % 2 === 0 && userAge >= 18) {
+//     alert('Welcome!')
+// } else {
+//     alert('You are not allowed to come in!')
+// }
+
+
+
+// N.3
+
+// let name = prompt('What is your name?').toUpperCase().trim()
+// let age = prompt('How old are you?').trim()
+
+// if (name[0] === 'A' && age >= 18) {
+//     alert('Welcome!')
+// } else {
+//     alert('You are not allowed to come in!')
+// }
+
+
+
+// N.4
+
+// let guests = 'Mirziyoyev Lukashenko Putin Zelenskiy Makron'
+// let presidents = prompt('What is your name?').trim().toUpperCase()
+// guests = guests.toUpperCase()
+// if (guests.includes(presidents)) {
+//     alert('Welcome Mr President!')
+// } else {
+//     alert('Mr President you are not invited!')
+// }
